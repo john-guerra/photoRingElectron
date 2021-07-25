3fd8c467682e8e9c13c585a709211374c912cdd9
Varshitha Uppula <varshithauppula@gmail.com>
2021-07-25 14:45:34 -0400

    sqlite connection

--- preload.js
+++ preload.js
@@ -2,26 +2,36 @@ const { contextBridge, ipcRenderer } = require("electron");
 
 // Expose protected methods that allow the renderer process to use
 // the ipcRenderer without exposing the entire object
-var Connection = require("tedious").Connection;  
-var config = {  
-  server: "localhost",  //update me
-  authentication: {
-    type: "default",
-    options: {
-      userName: "root", //update me
-      password: "123"  //update me
-    }
-  },
-  options: {
-    // If you are on Microsoft Azure, you need encryption:
-    encrypt: true,
-    database: "photoring2"  //update me
+// var Connection = require("tedious").Connection;
+// var config = {
+//   server: "localhost",  //update me
+//   authentication: {
+//     type: "default",
+//     options: {
+//       userName: "root", //update me
+//       password: "123"  //update me
+//     }
+//   },
+//   options: {
+//     // If you are on Microsoft Azure, you need encryption:
+//     encrypt: true,
+//     database: "photoring2"  //update me
+//   }
+// };
+//
+//
+// var Request = require("tedious").Request;
+// var TYPES = require("tedious").TYPES;
+
+
+var sqlite3 = require("sqlite3").verbose();
+// eslint-disable-next-line no-unused-vars
+var db = new sqlite3.Database("mydb.db",(err)=>{
+  if(err){
+    return console.error(err.message);
   }
-};  
-
-
-var Request = require("tedious").Request;  
-var TYPES = require("tedious").TYPES;  
+  console.log("connected to sqlite");
+});
 
 
 contextBridge.exposeInMainWorld("api", {
@@ -42,42 +52,62 @@ contextBridge.exposeInMainWorld("api", {
   ///
   insetExifData: (exifData) => {  
     console.log(exifData["DateTime"]);
-  
-    var connection = new Connection(config);  
-    connection.on("debug", function(err) { console.log("debug:", err);});
-    connection.connect();
-  
-    // eslint-disable-next-line no-undef
-    request = new Request("INSERT INTO photos VALUES ( @PHOTOID, @APERTURE, @BRIGHTNESS, @DATETIME, @FOCALLENGTH) ;", function(err) {
-      if (err) {  
-        console.log(err);}  
-    });  
-    // eslint-disable-next-line no-undef
-    request.addParameter("PHOTOID", TYPES.Int,1);//exifData["ApertureValue"][0]);
-    // eslint-disable-next-line no-undef
-    request.addParameter("APERTURE", TYPES.Float,2.0);//exifData["ApertureValue"][0]);
-    // eslint-disable-next-line no-undef
-    request.addParameter("BRIGHTNESS", TYPES.Float , exifData["BrightnessValue"]);
-    // eslint-disable-next-line no-undef
-    request.addParameter("DATETIME", TYPES.VarChar, exifData["DateTime"]);
-    // eslint-disable-next-line no-undef
-    request.addParameter("FOCALLENGTH", TYPES.Float, 4);
-    // eslint-disable-next-line no-undef
-    request.on("row", function(columns) {
-      columns.forEach(function(column) {  
-        if (column.value === null) {  
-          console.log("NULL");  
-        } else {  
-          console.log("Photo id is " + column.value);  
-        }  
-      });  
+    var db = new sqlite3.Database("mydb.db",(err)=>{
+      if(err){
+        return console.error(err.message);
+      }
+      console.log("connected to sqlite");
     });
-    // eslint-disable-next-line no-unused-vars,no-undef
-    request.on("requestCompleted", function (rowCount, more) {
-      connection.close();
-    });
+    // db.serialize(function() {
+    //   db.run("CREATE TABLE if not exists lorem (info TEXT)");
+    //
+    //   var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+    //   for (var i = 0; i < 10; i++) {
+    //     stmt.run("Ipsum " + i);
+    //   }
+    //   stmt.finalize();
+    //
+    //   db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
+    //     console.log(row.id + ": " + row.info);
+    //   });
+    // });
+    //
+    // db.close();
+    // var connection = new Connection(config);
+    // connection.on("debug", function(err) { console.log("debug:", err);});
+    // connection.connect();
+  
     // eslint-disable-next-line no-undef
-    connection.execSql(request);
+    // request = new Request("INSERT INTO photos VALUES ( @PHOTOID, @APERTURE, @BRIGHTNESS, @DATETIME, @FOCALLENGTH) ;", function(err) {
+    //   if (err) {
+    //     console.log(err);}
+    // });
+    // eslint-disable-next-line no-undef
+    // request.addParameter("PHOTOID", TYPES.Int,1);//exifData["ApertureValue"][0]);
+    // // eslint-disable-next-line no-undef
+    // request.addParameter("APERTURE", TYPES.Float,2.0);//exifData["ApertureValue"][0]);
+    // // eslint-disable-next-line no-undef
+    // request.addParameter("BRIGHTNESS", TYPES.Float , exifData["BrightnessValue"]);
+    // // eslint-disable-next-line no-undef
+    // request.addParameter("DATETIME", TYPES.VarChar, exifData["DateTime"]);
+    // // eslint-disable-next-line no-undef
+    // request.addParameter("FOCALLENGTH", TYPES.Float, 4);
+    // // eslint-disable-next-line no-undef
+    // request.on("row", function(columns) {
+    //   columns.forEach(function(column) {
+    //     if (column.value === null) {
+    //       console.log("NULL");
+    //     } else {
+    //       console.log("Photo id is " + column.value);
+    //     }
+    //   });
+    // });
+    // // eslint-disable-next-line no-unused-vars,no-undef
+    // request.on("requestCompleted", function (rowCount, more) {
+    //   connection.close();
+    // });
+    // eslint-disable-next-line no-undef
+
   },
   ///
 });
